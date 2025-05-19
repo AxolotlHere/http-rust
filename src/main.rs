84883cbf7dbfe6b249b993e3f0883690cd76e4c6
AxolotlHere <flamingbro15@gmail.com>
2025-05-19 18:44:34 +0530

    single thread

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::fs;
+use std::io::{BufReader, prelude::*};
+use std::net::{TcpListener, TcpStream};
+#[tokio::main]
+
+async fn main() {
+    let listen_ = TcpListener::bind("192.168.0.108:7878").unwrap(); //Result<> return
+
+    for i in listen_.incoming() {
+        let i = i.unwrap();
+
+        handle_conn(i);
+    }
+}
+
+fn handle_conn(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let req: String = buf_reader.lines().next().unwrap().unwrap();
+    println!("{req:#?}");
+
+    //sending response
+    if req == "GET / HTTP/1.1" {
+        let resp_status: &str = "HTTP/1.1 200 OK";
+        let html_content: String = fs::read_to_string("index.html").unwrap();
+        let content_len: usize = html_content.len();
+        stream
+            .write_all(
+                format!("{resp_status}\r\nContent-Length:{content_len}\r\n\r\n{html_content}")
+                    .as_bytes(),
+            )
+            .unwrap();
+    } else {
+        let resp_status: &str = "HTTP/1.1 404 NOT FOUND";
+        let html_content: String = "404 Page not found".to_string();
+        let content_len: usize = html_content.len();
+        stream
+            .write_all(
+                format!("{resp_status}\r\nContent-Length:{content_len}\r\n\r\n{html_content}")
+                    .as_bytes(),
+            )
+            .unwrap();
+    }
+}
